2cd79d92f7c33ab73f27fdeab40b53274f0197e0
SamRulapaugh <rulapaugh17@gmail.com>
2026-08-27 11:38:17 -0600

    New update

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const { getUsers, createUser, getUser } = require("../controllers/users");
 
 router.get("/", getUsers);
 router.get("/:userId", () => console.log("GET users"));
+router.get("/", getUser);
 router.post("/", createUser);
 
 module.exports = router;
