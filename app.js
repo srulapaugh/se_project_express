2952920e6a2b360586077d3c635c33fc521cc4cf
SamRulapaugh <rulapaugh17@gmail.com>
2026-09-10 13:51:09 -0600

    update

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ const app = express();
 
 const { PORT = 3001 } = process.env;
 
+app.use((req, res, next) => {
+  req.user = {
+    _id: "5d8b8592978f8bd833ca8133",
+  };
+
+  next();
+});
+
 app.use(express.json());
 
 app.use("/", mainRouter);
